2e2e17b7260902f861bd02eabc04026ac2b0219b
cinthiacm72 <cinthiacm@gmail.com>
2024-09-24 16:33:24 -0300

    new config

--- server/config.js
+++ server/config.js
@@ -1,6 +1,8 @@
-export const CLIENT_URL = process.env.CLIENT_URL || [
+/* export const CLIENT_URL = process.env.CLIENT_URL || [
   "http://localhost:5173",
   "http://localhost:5174",
-];
+]; */
+
+export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
 
 export const PORT = process.env.PORT || 3000;
--- server/index.js
+++ server/index.js
@@ -25,26 +25,26 @@ app.use(express.json());
 app.use(
   cors({
     origin: clientUrl,
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    /*     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE"], */
   })
 );
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", clientUrl);
-  res.header(
+  /*   res.header(
     "Access-Control-Allow-Methods",
     "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-  );
-  res.header(
+  ); */
+  /*  res.header(
     "Access-Control-Allow-Headers",
     "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-  );
-  res.header("Access-Control-Allow-Credentials", true);
+  ); */
+  /*   res.header("Access-Control-Allow-Credentials", true);
   if (req.method === "OPTIONS") {
     res.status(200).end();
     return;
-  }
+  } */
   next();
 });
 
