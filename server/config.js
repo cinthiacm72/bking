5013cb7b8af23a6fe5e299578c8d38c048c7980d
cinthiacm72 <cinthiacm@gmail.com>
2024-10-30 16:09:27 -0300

    Elimiar server/config del indice

--- server/config.js
+++
@@ -1,8 +0,0 @@
-/* export const CLIENT_URL = process.env.CLIENT_URL || [
-  "http://localhost:5173",
-  "http://localhost:5174",
-]; */
-
-export const CLIENT_URL = process.env.CLIENT_URL || "http://localhost:5173";
-
-export const PORT = process.env.PORT || 3000;
